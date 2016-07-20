a49d8dbf3d10ffe02e184a2f92e78d364aa5cac3
Reed N <rbnorstrom@pluto.dsu.edu>
2016-07-19 21:37:20 -0500

    Reed Compare
    Compare a vector and a bitmap and recieve a percentage correct score

--- app/src/main/java/com/caos/pure/japlearninggame/Utility/compareBitmapVector.java
+++ app/src/main/java/com/caos/pure/japlearninggame/Utility/compareBitmapVector.java
@@ -1,6 +1,12 @@
 package com.caos.pure.japlearninggame.Utility;
 
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.VectorDrawable;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * This object takes a bitmap and a vector and sees how well the bitmap overlaps the vector and scores the outcome
@@ -8,8 +14,18 @@ import android.graphics.Bitmap;
  */
 public class compareBitmapVector {
 
+    @NonNull
     private Bitmap drawn;
+    @NonNull
     private Object hiragana;
+
+    //will be removed when hiragana object is provided
+    private VectorDrawable temp;
+
+    private byte[] drawnImage;
+    private byte[] savedImage;
+
+    private double savedScore;
 
     public compareBitmapVector(Bitmap drawn, Object curHiragana) {
         this.drawn = drawn;
@@ -20,26 +36,58 @@ public class compareBitmapVector {
      * @param1 a bitmap
      * @param2 a hiragana object
      *
-     * @return the percentage correct
+     * @return the percentage correct of pixels
      */
     public double score(){
-        double percent = 0.00;
+        int marks = 0;
+        int size = drawn.getByteCount();
 
+        createComparableDrawnImage();
+
+        createComparableSavedImage();
+
+        if(drawnImage.length == savedImage.length) {
+            for (int loc = 0; loc > size; loc++) {
+                if (!(drawnImage[loc] == '0')) {   //not white
+                    if (savedImage[loc] == '1') {  //is black
+                        marks++;
+                    }
+                }
+            }
+        } else {
+            Log.e("ERROR", "Images not same size");
+        }
+
+        double percent = marks / size;
+        this.savedScore = percent;
         return percent;
     }
 
-    public Bitmap getDrawn() {
-        return drawn;
+    /**
+     * @return saves a JPEG byte[] from bitmap
+     */
+    private void createComparableDrawnImage(){
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        drawn.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        this.drawnImage = stream.toByteArray();
     }
 
-    public void setDrawn(Bitmap drawn) {
-        this.drawn = drawn;
-    }
-    public Object getHiragana() {
-        return hiragana;
+    /**
+     * @return saves a JPEG byte[] from vectorDrawable
+     */
+    private void createComparableSavedImage(){
+        temp.setBounds(drawn.getWidth()/2, drawn.getHeight()/2, drawn.getWidth()/2, drawn.getHeight()/2);
+        Bitmap tempBitMap = ((BitmapDrawable)temp.getCurrent()).getBitmap();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        tempBitMap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        this.savedImage = stream.toByteArray();
+
     }
 
-    public void setHiragana(Object hiragana) {
-        this.hiragana = hiragana;
+    /**
+     * @return the calculated score from above
+     */
+    public double getSavedScore() {
+        return savedScore;
     }
 }
