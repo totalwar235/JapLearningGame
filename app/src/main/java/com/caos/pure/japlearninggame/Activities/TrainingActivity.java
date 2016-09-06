6689bd24f3964b9d22f00f0acdec8f1df2d994e4
Koji <a.cz0512@gmail.com>
2016-09-05 20:06:18 -0500

    Add ImageView, hiraganaImage

--- app/src/main/java/com/caos/pure/japlearninggame/Activities/TrainingActivity.java
+++ app/src/main/java/com/caos/pure/japlearninggame/Activities/TrainingActivity.java
@@ -1,9 +1,11 @@
 package com.caos.pure.japlearninggame.Activities;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.caos.pure.japlearninggame.R;
 import com.caos.pure.japlearninggame.Utility.Hiragana;
@@ -13,6 +15,7 @@ import com.caos.pure.japlearninggame.Utility.loadHiragana;
 public class TrainingActivity extends AppCompatActivity {
 
     private TrainingView drawingView;
+    private ImageView hiraganaImage;
     private Hiragana trainer;
 
     @Override
@@ -20,6 +23,7 @@ public class TrainingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.training_view);
         drawingView = (TrainingView) findViewById(R.id.trainingView);
+        hiraganaImage = (ImageView) findViewById(R.id.hiraganaImage);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -29,6 +33,7 @@ public class TrainingActivity extends AppCompatActivity {
             //this will get the vector from the res dynamically for the training view
             this.trainer = loadHiragana.select(hiraganaName);
             trainer.getVector(drawingView.getContext());
+            hiraganaImage.setImageDrawable(trainer.getVector());
         }
     }
 
